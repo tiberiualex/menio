9e5cde0e483248ce507ceb97416395aac04c0160
tiberiualex <tiberiu.alexander@gmail.com>
2015-10-18 17:47:25 +0100

    Added object constructor for the menu

--- js/menio.js
+++ js/menio.js
@@ -1,3 +1,33 @@
-var menio = function(options) {
-  var menu = document.querySelector('.menio');
+;(function() {
+  'use strict';
+
+  var menio = function menio(options) {
+    this.element = document.querySelector(options.element);
+    this.breakpoint = options.breakpoint;
+    this.init();
+  }
+
+  window['menio'] = menio;
+
+  menio.prototype.init = function() {
+    this.switchView();
+
+    window.addEventListener('resize', this.switchView.bind(this));
+  };
+
+  menio.prototype.switchView = function() {
+    if (window.innerWidth() < this.breakpoint) {
+      this.element.classList.add('mobile');
+    }
+    else {
+      this.element.classList.removeClass('mobile');
+    }
+  };
+});
+
+var parametru = {
+  element: '.menio',
+  breakpoint: 800
 }
+
+var meniu = new menio(parametru);
